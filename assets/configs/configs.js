481c96ef26cac035b386bc1c6e216cc7d017669a
davmaene <davidmened@gmail.com>
2023-11-27 10:54:29 +0200

    up version done

--- assets/configs/configs.js
+++ assets/configs/configs.js
@@ -1,5 +1,5 @@
-export const endpoint = "https://hewaback.kivugreen.cd" //"http://192.168.1.178:3001"// 
-export const endpointcollector = "https://backmarket.kivugreen.cd"
+export const endpoint = "http://192.168.1.178:3001";//"https://hewaback.kivugreen.cd"// 
+export const endpointcollector = "https://backmarket.kivugreen.cd";
 export const appname = "HewAgri";
 export const umergencyphonenumber = "+243995517430";
 export const appcompanyname = "Kivu Green Corporation.";
